e1a1802c689de393b58c3c12aef1e44e957d3ce5
Livestronger08 <derek.downie@ttu.edu>
2020-03-04 22:06:46 +0000

    Lesson 24 Video Uploading

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_03_180257) do
+ActiveRecord::Schema.define(version: 2020_03_04_214838) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,6 +32,7 @@ ActiveRecord::Schema.define(version: 2020_03_03_180257) do
     t.integer "section_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "video"
     t.index ["section_id"], name: "index_lessons_on_section_id"
   end
 
