ee5b9e84b0e1a90664cb3801881a79c61e760aee
Livestronger08 <derek.downie@ttu.edu>
2020-03-06 17:49:22 +0000

    Lesson 28 Require Users to be Enrolled in a Course to View the Lessons

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,8 +1,16 @@
 class LessonsController < ApplicationController
+    before_action :authenticate_user!
+    before_action :require_authorized_for_current_lesson.section.course, only: [:show]
     def show
     end
 
     private
+
+    def require_authorized_for_current_lesson_section_course
+        if current_lesson.section.course != current_user.enrolled_in?
+            redirect_to lesson_path, alert: 'Unauthorized'
+        end
+    end 
 
     helper_method :current_lesson
     def current_lesson
